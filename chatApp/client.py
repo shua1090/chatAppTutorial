13d34d363bc53d54c14cb5aae339572982fd7142
shua1090 <shynn.lawrence@gmail.com>
2021-02-10 19:46:29 -0800

    Initial Commit

---
+++ chatApp/client.py
@@ -0,0 +1,73 @@
+import socket
+import tkinter as tk
+from tkinter.constants import END
+from message import Message
+import pickle
+import threading
+
+global entry, msgBox
+
+def sendMessage():
+    mess = Message(
+        userName, entry.get()
+    )
+    
+    msgBox.insert(
+        END,
+        "\n"+"%s says %s"%(mess.sender, mess.text)
+    )
+
+    entry.delete(0, END)
+    
+    s.send(
+        pickle.dumps(mess, protocol=3)
+    )
+
+    print("Finished Sending Message")
+
+
+
+def receiveMessage():
+    while True:
+        receivedMessage = s.recv(512)
+        receivedMessage = pickle.loads(receivedMessage)
+        msgBox.insert(
+            END,
+            "\n"+"%s says %s"%(receivedMessage.sender, receivedMessage.text)
+        )
+
+def setupGUI():
+    window = tk.Tk()
+
+    window.geometry("300x300")
+
+    title = tk.Label(window, text="Chat Application", font=("Arial", 14, "bold"))
+    title.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
+
+    global entry, msgBox
+
+    entry = tk.Entry(window, width=30)
+    entry.place(relx=0.4, rely=0.9, anchor=tk.CENTER)
+
+
+    send = tk.Button(window, text="Send", font=("Arial", 10), command=sendMessage)
+    send.place(relx=0.8, rely=0.9, anchor=tk.CENTER)
+
+    msgBox = tk.Text(window, width=35, height=10)#, text="Hello World")
+    msgBox.place(relx = 0.5, rely= 0.5, anchor="center")
+
+    window.mainloop()
+
+
+
+if __name__ == "__main__":
+
+    userName = input("What is your username? ")
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    port = 5000
+    s.connect(('192.168.86.110', port))
+
+    threading.Thread(target=receiveMessage,).start()
+
+    setupGUI()
---
+++ chatApp/message.py
@@ -0,0 +1,4 @@
+class Message:
+    def __init__(self, sender:str, text:str):
+        self.sender = sender
+        self.text = text
